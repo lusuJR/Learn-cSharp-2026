bdc83256189ec59eb00771c2a137c66ff994d3a5
Selemani <lusukama.s@gmail.com>
2026-05-05 03:40:25 +0200

    add Example-0

---
+++ Example-0-HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example_0_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour Mr. Lusukama");
+        }
+    }
+}
